211062acef7f7c9d41c74efb74cf319e819b1fc8
congliuUvA <c.liu4@uva.nl>
2023-01-18 17:26:40 +0100

    change the rotation to box instead of atoms

--- Exercise/voxel_rotate_atom.py
+++ Exercise/voxel_rotate_atom.py
@@ -18,27 +18,23 @@ len_of_voxel = 0.4
 def range_editor(coord: ndarray) -> List:
     """This function edits the range of the coordinates,
     given the central position of the ca atom under the cartesian coordinates.
-
     Args:
         coord: central ca coords.
-
     Returns:
         range_list: list of x,y,z range
     """
     x, y, z = coord[0], coord[1], coord[2]
-    range_x = [x - num_of_voxels / 2 * len_of_voxel, x + num_of_voxels / 2 * len_of_voxel]
-    range_y = [y - num_of_voxels / 2 * len_of_voxel, y + num_of_voxels / 2 * len_of_voxel]
-    range_z = [z - num_of_voxels / 2 * len_of_voxel, z + num_of_voxels / 2 * len_of_voxel]
+    range_x = [x - num_of_voxels/2 * len_of_voxel, x + num_of_voxels/2 * len_of_voxel]
+    range_y = [y - num_of_voxels/2 * len_of_voxel, y + num_of_voxels/2 * len_of_voxel]
+    range_z = [z - num_of_voxels/2 * len_of_voxel, z + num_of_voxels/2 * len_of_voxel]
     range_list = [range_x, range_y, range_z]
     return range_list
 
 
 def generate_central_atoms(struct: Bio.PDB.Structure.Structure) -> Tuple[List, List]:
     """This function generates central atom lists and their corresponding ranges.
-
     Args:
         struct: structure in biopython.
-
     Returns:
         ca_list
         cb_list
@@ -60,70 +56,21 @@ def generate_central_atoms(struct: Bio.PDB.Structure.Structure) -> Tuple[List, L
     return ca_list, cb_list
 
 
-def generate_vertices(central_coord: np.array, rot: np.array) -> np.array:
-    """This function generates rotated coordinates of vertices of box
-        according to the coordinate of the central position.
-
-    Args:
-        central_coord: coordinate of the central position of the box.
-        rot: rotation matrix that rotation corner vector to ca_cb direction.
-
-    Returns:
-        vetices_coords: coordinates of 8 vertices.
-    """
-    left_down_corner = central_coord - num_of_voxels / 2 * len_of_voxel
-    factor_list = np.array(list(product("01", repeat=3))).astype(np.int32)
-    vertices_coords = factor_list * num_of_voxels * len_of_voxel + left_down_corner
-    vertices_centre_vector = vertices_coords - central_coord
-    # vector addition, (8, 3)
-    vertices_coords = (vertices_centre_vector@rot.T) + central_coord
-    return vertices_coords
-
-
-def is_in_range(vertices_coords: np.array, atom: Bio.PDB.Atom.Atom, center_coord: np.array) -> bool:
-    """This function determines whether the given atom is inside the range of the box.
-
-    Args:
-        vertices_coords: rotated coordinates of 8 vertices.
-        atom: selected atom.
-        center_coord: centre coordinate of the box
-    """
-    length_of_box = num_of_voxels * len_of_voxel
-    atom_center_vector = atom.coord - center_coord
-    vector_coord_diff = vertices_coords - vertices_coords[0]
-    x_vector, y_vector, z_vector = vector_coord_diff[1], vector_coord_diff[2], vector_coord_diff[4]
-    # x_vector = x_vector / np.linalg.norm(x_vector)
-    # y_vector = y_vector / np.linalg.norm(y_vector)
-    # z_vector = z_vector / np.linalg.norm(z_vector)
-    #
-    # in_range = (abs(atom_center_vector @ x_vector) * 2 < length_of_box) and \
-    #            (abs(atom_center_vector @ y_vector) * 2 < length_of_box) and \
-    #            (abs(atom_center_vector @ z_vector) * 2 < length_of_box)
-
-    xyz_mat = np.array([x_vector, y_vector, z_vector])
-    xyz_mat = xyz_mat / np.linalg.norm(xyz_mat, axis=1)
-    in_range = np.all(xyz_mat@atom_center_vector * 2 < length_of_box)
-
-    return in_range
-
-
 def select_in_range_atoms(
         struct, ca_list, cb_list,
         selected_central_atom_type="CA", shift=0
-) -> Tuple[List, List, np.array]:
+) -> Tuple[List, List, List]:
     """This function generates the selected in-range atom corresponding to the central atom ["CA", "CB"].
-
     Args:
         struct: structure in biopython.
         ca_list: list containing CA in the PDB file.
         cb_list: list containing CB in the PDB file.
         selected_central_atom_type: string stating which central atom is selected.
         shift: shift of carbon alpha along the diagonal direction.
-
     Returns:
         voxel_atom_lists: list of atoms contained in each 20*20*20 voxels.
+        rot_mat: list of rotation matrix corresponding to every residue.
         central_atom_coords: list of central atom coordinate after applying shift.
-        vertices_coords: np.array of 8 vertices coordinates of voxels. (num_of_res, (8, 3))
     """
     central_atom_list = ca_list if selected_central_atom_type == "CA" else cb_list
 
@@ -134,12 +81,10 @@ def select_in_range_atoms(
     voxel_atom_lists = []
 
     # search atoms that are within the range
-    # This function takes time to initialize, so only initialize once outside the loop, the coordinates
-    # of the atoms should not be transformed but only using rotation matrix to left-multiply.
     nb_search = NeighborSearch(list(struct.get_atoms()))
 
-    # record rotated coordinates of vertices
-    vertices_coords = []
+    # record rotation matrix for every residue.
+    rot_mats = []
 
     # record central atom coordinates
     central_atom_coords = []
@@ -149,15 +94,13 @@ def select_in_range_atoms(
         # the first atom in the voxel is the central atom.
         voxel_atom_list = [central_atom]
 
-        # generate rotation matrix, rotate corner_vector to ca_cb_vector
+        # generate rotation matrix
+        # same rotation for all the atoms from one residue
         rot = np.identity(3) if selected_central_atom_type == "CA" else rotmat(corner_vector, ca_cb_vector)
 
         # Shift of Ca position can be applied by confirming the shift of the central atom, i.e. by confirming the new
         # central atom coordinates, vector subtraction
         central_atom_coord = central_atom.get_coord() - shift * ca_cb_vector.normalized().get_array()
-
-        # generate eight rotated box vertices, (8, 3)
-        vertices_coord = generate_vertices(central_atom_coord, rot)
 
         # search in-sphere atoms
         # It doesn't matter if not transferring atom coordinate here because sphere is invariant.
@@ -165,54 +108,58 @@ def select_in_range_atoms(
 
         # keep in-range atoms
         for atom in searched_atom_list:
-            # determine whether the selected atom is in range of the rotated box.
-            bool_is_in_range = is_in_range(vertices_coord, atom, central_atom_coord)
-            if bool_is_in_range and atom != central_atom and atom.parent != central_atom.parent:
-                voxel_atom_list.append(atom)
-            if atom.parent == central_atom.parent and not bool_is_in_range:
+            # remember to left-multiply rotation matrix all the time when requiring atom coordinates.
+            central_range_list = range_editor(rot @ central_atom_coord)
+            atom_coord = rot @ atom.get_coord()
+            range_x, range_y, range_z = central_range_list[0], central_range_list[1], central_range_list[2]
+            atom_x, atom_y, atom_z = atom_coord[0], atom_coord[1], atom_coord[2]
+            if range_x[0] < atom_x < range_x[1] and \
+                    range_y[0] < atom_y < range_y[1] and \
+                    range_z[0] < atom_z < range_z[1]:
+                if atom != central_atom and atom.parent != central_atom.parent:
+                    voxel_atom_list.append(atom)
+
+            if atom.parent == central_atom.parent and not (
+                    range_x[0] < atom_x < range_x[1] and
+                    range_y[0] < atom_y < range_y[1] and
+                    range_z[0] < atom_z < range_z[1]
+            ):
                 raise ValueError("Shift value needs to be justified,"
                                  "atoms from central residual is not included in the box! "
                                  "Please use a different shift value.")
-        # record atoms that are within the range
+
         voxel_atom_lists.append(voxel_atom_list)
+
+        # save rotation matrix
+        rot_mats.append(rot)
 
         # save central atom coordinate
         central_atom_coords.append(central_atom_coord)
 
-        # save vertices coordinates for filling the atom in voxels.
-        vertices_coords.append(vertices_coord)
-
-    return voxel_atom_lists, central_atom_coords, np.array(vertices_coords)
+    return voxel_atom_lists, rot_mats, central_atom_coords
 
 
-def generate_voxel_atom_lists(struct: Bio.PDB.Structure.Structure) -> Tuple[List, List, np.array]:
+def generate_voxel_atom_lists(struct: Bio.PDB.Structure.Structure) -> Tuple[List, List, List]:
     """This function generates permitted range for every selected CA. (20 * 20 * 20), 0.8 A for dimension.
-
     Args:
         struct: structure in biopython.
-
-    Returns:
-        voxel_atom_lists: list of atoms contained in each 20*20*20 voxels.
-        central_atom_coords: list of central atom coordinate after applying shift.
-        vertices_coords: np.array of 8 vertices coordinates of voxels. (num_of_res, (8, 3))
     """
     ca_list, cb_list = generate_central_atoms(struct)
-    voxel_atom_lists, central_atom_coords, vertices_coords = select_in_range_atoms(
+    voxel_atom_lists, rot_mats, central_atom_coords = select_in_range_atoms(
         struct, ca_list, cb_list, selected_central_atom_type="CB", shift=0
     )
-    return voxel_atom_lists, central_atom_coords, vertices_coords
+    return voxel_atom_lists, rot_mats, central_atom_coords
 
 
 def generate_selected_element_voxel(
-        selected_element: str, voxel_atom_list: List, central_atom_coord: np.array, vertices_coord: np.array,
+        selected_element: str, voxel_atom_list: List, rot_mat: np.array, central_atom_coord: np.array
 ):
     """This function generate selected-element voxel by inserting True/False to 20*20*20 voxel.
-
     Args:
         selected_element: selected element for one single channel, "C", "N", "O", "S".
         voxel_atom_list: The pre-generated voxel list containing central CA atoms and the surrounding atoms.
+        rot_mat: rotation matrix for the selected residue.
         central_atom_coord: list of central atom coordinate after applying shift.
-        vertices_coord: coordinates of 8 vertices of the box for the selected residue.
     """
     if selected_element not in ["C", "N", "O", "S"]:
         raise ValueError("'selected_element' has to be in the options of 'C', 'N', 'S', 'O'")
@@ -221,41 +168,45 @@ def generate_selected_element_voxel(
     voxels_bool = (np.ones((num_of_voxels, num_of_voxels, num_of_voxels)) == 0).astype(bool)
 
     # 2. selected corresponding atoms
-    selected_atom_list = []
-    for atom in voxel_atom_list:
+    selected_atom_list = [voxel_atom_list[0]]
+    for atom in voxel_atom_list[1:]:
         if atom.element == selected_element and atom.element in ["C", "N", "O", "S"]:
             selected_atom_list.append(atom)
 
     # 3. Generate the coordinates of 20 * 20 * 20 voxels,
-    # one centric coordinate for one sub voxel, (20, 20, 20, 3)
-    left_down_corner_coord, right_up_corner_coord = vertices_coord[0], vertices_coord[-1]  # left-down corner
-    x_split = np.linspace(left_down_corner_coord[0], right_up_corner_coord[0], num=num_of_voxels + 1)[
-              :-1] + len_of_voxel / 2
-    y_split = np.linspace(left_down_corner_coord[1], right_up_corner_coord[1], num=num_of_voxels + 1)[
-              :-1] + len_of_voxel / 2
-    z_split = np.linspace(left_down_corner_coord[2], right_up_corner_coord[2], num=num_of_voxels + 1)[
-              :-1] + len_of_voxel / 2
-    # centre coordinates of sub voxels (20, 20, 20, 3)
-    sub_voxel_centre_coords = [[[[x_split[i], y_split[j], z_split[k]]
-                                 for i in range(num_of_voxels)]
-                                for j in range(num_of_voxels)]
-                               for k in range(num_of_voxels)]
-    sub_voxel_centre_coords = np.array(sub_voxel_centre_coords)
+
+    # First method
+    # 8 coordinates of 8 vertex for 1 voxel, (20, 20, 20, 8, 3)
+    # initial_atom_coord = rot_mat@central_atom_coord - 10 * 0.8
+    # voxels_coords = [[[gen_one_voxel_coords(
+    #     np.array([[initial_atom_coord[0] + i * 0.8],
+    #              [initial_atom_coord[1] + j * 0.8],
+    #              [initial_atom_coord[2] + k * 0.8]])) for i in range(20)] for j in range(20)] for k in range(20)]
+
+    # Second method
+    # 1 centric coordinate for 1 voxel, (20, 20, 20, 3)
+    initial_atom_coord = rot_mat @ central_atom_coord - num_of_voxels/2 * len_of_voxel + len_of_voxel/2
+    voxels_coords = [[[[
+        initial_atom_coord[0] + i * len_of_voxel,
+        initial_atom_coord[1] + j * len_of_voxel,
+        initial_atom_coord[2] + k * len_of_voxel]
+        for i in range(num_of_voxels)]
+        for j in range(num_of_voxels)]
+        for k in range(num_of_voxels)]
+    voxels_coords = np.array(voxels_coords)
 
     # 4. Add atom to the voxel.
     for atom in selected_atom_list:
-        atom_coord = atom.coord
-        voxels_bool = add_atom_to_voxel(atom, atom_coord, sub_voxel_centre_coords, voxels_bool)
+        atom_coord = rot_mat @ atom.coord
+        voxels_bool = add_atom_to_voxel(atom, atom_coord, voxels_coords, voxels_bool)
     return voxels_bool
 
 
 def gen_one_voxel_coords(coord: np.array) -> np.array:
     """This function generates the eight coordinates
     of one single voxel given the coordinates of left corner of the voxel.
-
     Args:
         coord: coordinate of the most left and inner vertex of the voxel.
-
     Returns:
         voxel_coords: voxel coordinates of eight vertices.
     """
@@ -268,33 +219,38 @@ def gen_one_voxel_coords(coord: np.array) -> np.array:
 def add_atom_to_voxel(
         atom: Bio.PDB.Atom.Atom,
         atom_coord: np.array,
-        sub_voxel_centre_coords: ndarray,
+        voxels_coords: ndarray,
         voxels_bool: ndarray,
 ) -> ndarray:
     """This function adds atom to the corresponding location of the voxel.
-
     Args:
         atom: atom object to be added
         atom_coord: atom coordinate of atom to be added
-        sub_voxel_centre_coords: coordinates of the 20*20*20 voxels.
+        voxels_coords: coordinates of the 20*20*20 voxels.
         voxels_bool: voxel array in bool.
-
     Returns:
         voxels_bool: voxel filled in new bool values.
     """
 
     radius_table = {"C": 0.70, "N": 0.65, "O": 0.60, "S": 1.00}
-    dists_element_voxels_coords = np.sqrt(np.sum((sub_voxel_centre_coords - atom_coord) ** 2, axis=-1))
+
+    dists_element_voxels_coords = np.sqrt(np.sum((voxels_coords - atom_coord) ** 2, axis=-1))
     # (20, 20, 20)
-    add_bool = np.where(dists_element_voxels_coords < radius_table[atom.element])
+    contact_element_voxel_coords = np.where(dists_element_voxels_coords < radius_table[atom.element])
+    add_bool = contact_element_voxel_coords
     voxels_bool[add_bool[0].T, add_bool[1].T, add_bool[2].T] = True
+
+    # (20, 20, 20, 8)
+    # only care about the previous three arrays, which represent the coordinate the of voxels, for the last array, it
+    # represents the index of the vertex that has been occupied.
+    # add_bool = np.unique(np.array(contact_element_voxel_coords)[:-1].T, axis=0)  # (3, number_of_occupied_voxel)
+    # voxels_bool[add_bool[0].T, add_bool[1].T, add_bool[2].T] = True
 
     return voxels_bool
 
 
 def visualize_voxels(voxel_list: List):
     """This function visualizes the voxels of CNOS.
-
     Args:
         voxel_list: a list include all voxels of 4 channels.
     """
@@ -315,20 +271,20 @@ def main():
     struct = load_protein("3gbn.pdb")
 
     # 1. generate atom lists for 20*20*20 voxels
-    voxel_atom_lists, central_atom_coords, vertices_coords = generate_voxel_atom_lists(struct)
+    voxel_atom_lists, rot_mats, central_atom_coords = generate_voxel_atom_lists(struct)  # (num_ca, num_atoms_in_voxel)
 
     # 2. take one voxel as an example.
     example_index = 0
     (
-        voxel_atom_list, central_atom_coord, vertices_coord
-    ) = (voxel_atom_lists[example_index], central_atom_coords[example_index], vertices_coords[example_index])
+        voxel_atom_list, rot_mat, central_atom_coord
+    ) = (voxel_atom_lists[example_index], rot_mats[example_index], central_atom_coords[example_index])
+
     # 3. iterate through ["C", "N", "O", "S"]
     elements = ["C", "N", "O", "S"]
+
     all_voxel = []
     for element in elements:
-        selected_element_voxel = generate_selected_element_voxel(
-            element, voxel_atom_list, central_atom_coord, vertices_coord
-        )
+        selected_element_voxel = generate_selected_element_voxel(element, voxel_atom_list, rot_mat, central_atom_coord)
         all_voxel.append(selected_element_voxel)
 
     # 4. visualization
