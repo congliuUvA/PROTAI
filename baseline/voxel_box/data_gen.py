b92ea1fe40acf90de3703ed0a4978c92c7acd0a3
congliuUvA <c.liu4@uva.nl>
2023-01-28 23:09:05 +0100

    typo

--- baseline/voxel_box/data_gen.py
+++ baseline/voxel_box/data_gen.py
@@ -70,7 +70,7 @@ def data_gen(args: DictConfig):
         # assign pdb info to args_voxel_box
         args_voxel_box.pdb_name = Path(pdb_unzip).stem
         args_voxel_box.pdb_path = pdb_unzip
-        task = gen_voxgiel_box_file.remote(args_voxel_box)
+        task = gen_voxel_box_file.remote(args_voxel_box)
 
         # remove generated pdb file, clean up the mess
         # os.system(f"rm {pdb_unzip}")
