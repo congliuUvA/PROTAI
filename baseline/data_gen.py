934aed2612f0efe7dfbe83b7e841609341076558
congliuUvA <c.liu4@uva.nl>
2023-02-07 21:10:04 +0100

    regenerate data using 3 nodes, 2/3

--- baseline/data_gen.py
+++ baseline/data_gen.py
@@ -51,18 +51,23 @@ def data_gen(args: DictConfig):
     logger.info("Start ray tasks.")
     tasks = []
     for pdb in raw_pdb_dir.rglob("*.gz"):
-        if idx > int(total / 3):
-            print("1/3 completed")
-            break
-        # if idx <= int(total / 3):
-        #     idx += 1
-        #     continue
-        # if idx <= int(total / 3):
-        #     idx += 1
-        #     continue
-        # if idx > 2*int(total / 3):
-        #     print("2/3 completed")
+        # 1/3
+        # if idx > int(total / 3):
+        #     print("1/3 completed")
         #     break
+
+        # 2/3
+        if idx <= int(total / 3):
+            idx += 1
+            continue
+        if idx > 2*int(total / 3):
+            print("2/3 completed")
+            break
+
+        # # 3/3
+        # if idx <= 2*int(total / 3):
+        #     idx += 1
+        #     continue
         # unzipped pdb file name
         pdb_pure_id = pdb.name.split(".")[0]
         assembly_id = pdb.name.split(".")[1]
@@ -97,5 +102,5 @@ def data_gen(args: DictConfig):
 if __name__ == "__main__":
     logger.info("Data gen started!")
     if not ray.is_initialized():
-        ray.init(address='10.150.1.8:6379')
+        ray.init(address='10.150.1.1:6379')
     data_gen()
