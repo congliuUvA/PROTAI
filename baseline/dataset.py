4cd46f07b5e18a7f78e1730d0d696de542c4b25c
congliuUvA <c.liu4@uva.nl>
2023-02-07 18:25:02 +0100

    debug dataset.py

--- baseline/dataset.py
+++ baseline/dataset.py
@@ -42,22 +42,22 @@ class VoxelsDataset(Dataset):
         if self.training:
             assert fold is not None
             self.dataset_csv = self.dataset_csv.loc[self.dataset_csv["fold"] == fold]
-        # length accumulate list, prepared for indexing specific box in the data set.
-        self.len_accumulate_list = []
         self.residue_name = [
             "ALA", "ARG", "ASN", "ASP", "CYS",
             "GLU", "GLN", "GLY", "HIS", "ILE",
             "LEU", "LYS", "MET", "PHE", "PRO",
             "SER", "THR", "TRP", "TYR", "VAL"
         ]
-        self.residue_name_dic = {self.residue_name[i]: idx for idx, i in enumerate(self.residue_name)}
+        self.residue_name_dic = {name: idx for idx, name in enumerate(self.residue_name)}
 
-    def len(self) -> int:
+    def __len__(self) -> int:
         """Return the length of the dataset.
 
         Returns:
             length of directory.
         """
+        # length accumulate list, prepared for indexing specific box in the data set.
+        self.len_accumulate_list = []
         length = 0
         # iterate through all files in sub set
         for idx, row in self.dataset_csv.iterrows():
@@ -67,13 +67,11 @@ class VoxelsDataset(Dataset):
             hdf5_file = f"{pdb_id}_pdb1.hdf5"
             f = h5py.File(hdf5_file, "r")
             # each box count as one data sample
-            length += len([box for box in f[chain]])
-            last_len = 0 if len(self.len_accumulate_list) == 0 else self.len_accumulate_list[-1]
-            self.len_accumulate_list.append(length + last_len)
-        self.len_accumulate_list = np.array(self.len_accumulate_list)
+            length += len(f[chain].keys())
+            self.len_accumulate_list.append(length)
         return length
 
-    def get(self, idx: int) -> Tuple[Tensor, str]:
+    def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor]:
         """Load the data corresponding to the given index.
 
         Args:
@@ -84,17 +82,20 @@ class VoxelsDataset(Dataset):
         """
         # given one specific data instance idx, find the corresponding position in
         # accumulated length list, the idx + 1 represents the index of the row in the csv file
-        row_idx = np.where(self.len_accumulate_list < idx)[0][-1] + 1
+        row_idx = np.where(np.array(self.len_accumulate_list) <= idx)[0][-1] + 1 \
+            if idx >= self.len_accumulate_list[0] else 0
         # use the given idx to minus the last accumulated length to derive the residual index in
         # the row hdf5 file
-        residue_idx = idx - self.len_accumulate_list[row_idx - 1]
+        residue_idx = idx - self.len_accumulate_list[row_idx - 1] if row_idx > 0 else idx
         row = self.dataset_csv.iloc[row_idx]
         hdf5_file = f"{row.id}_pdb1.hdf5"
         chain_id = row.full_id.split("_")[-1]
         f = h5py.File(hdf5_file, "r")
         data = f[chain_id][list(f[chain_id].keys())[residue_idx]]
-        label = self.residue_name_dic[str(data.attrs["residue_name"])]
-        voxel = torch.tensor(data[:]) + 0
+        label_idx = self.residue_name_dic[str(data.attrs["residue_name"])]
+        label = torch.zeros(len(self.residue_name))
+        label[label_idx] = 1.0
+        voxel = (torch.tensor(data[:]) + 0).type(torch.FloatTensor)
         return voxel, label
 
 
