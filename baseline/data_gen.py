4a952c787fa8fd5ea9648a02fb9e20089c8a9083
congliuUvA <c.liu4@uva.nl>
2023-02-08 11:54:25 +0100

    run 2/4 on ivi-cn011

--- baseline/data_gen.py
+++ baseline/data_gen.py
@@ -51,18 +51,18 @@ def data_gen(args: DictConfig):
     logger.info("Start ray tasks.")
     tasks = []
     for pdb in raw_pdb_dir.rglob("*.gz"):
-        # 1/4
-        if idx > int(total / 4):
-            print("1/4 completed")
-            break
-
-        # # 2/4
-        # if idx <= int(total / 4):
-        #     idx += 1
-        #     continue
-        # if idx > 2*int(total / 4):
-        #     print("2/4 completed")
+        # # 1/4
+        # if idx > int(total / 4):
+        #     print("1/4 completed")
         #     break
+
+        # 2/4
+        if idx <= int(total / 4):
+            idx += 1
+            continue
+        if idx > 2*int(total / 4):
+            print("2/4 completed")
+            break
 
         # 3/4
         # if idx <= 2*int(total / 4):
@@ -111,5 +111,5 @@ def data_gen(args: DictConfig):
 if __name__ == "__main__":
     logger.info("Data gen started!")
     if not ray.is_initialized():
-        ray.init(address='10.150.1.16:6379')
+        ray.init(address='10.150.1.11:6379')
     data_gen()
