65471f8c542c82107c5151f3b43a4eb89360bb1b
congliuUvA <c.liu4@uva.nl>
2023-02-15 23:44:58 +0100

    examine first half

--- baseline/data_gen.py
+++ baseline/data_gen.py
@@ -51,15 +51,17 @@ def data_gen(args: DictConfig):
     logger.info("Start ray tasks.")
     tasks = []
     for pdb in raw_pdb_dir.rglob("*.gz"):
-        # # 1/2
-        if idx > int(total / 2):
-            print("1/2 completed")
-            break
+        # # # 1/2
+        # if idx > int(total / 2):
+        #     print("1/2 completed")
+        #     break
 
-        # 2/2
-        # if idx <= int(total / 2):
-        #     idx += 1
-        #     continue
+        # # 2/2
+        if idx <= 13000:
+            idx += 1
+            continue
+        if idx > 14487:
+            break
 
         # unzipped pdb file name
         pdb_pure_id = pdb.name.split(".")[0]
