48120b90489c1afc72df5cddb7643ce5b7cb52a4
congliuUvA <c.liu4@uva.nl>
2023-02-23 11:47:40 +0100

    data_gen sh 3

---
+++ baseline/data_gen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --job-name=data_generation_3
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=46
+#SBATCH --mem=64G
+#SBATCH --time=7-0
+#SBATCH -o data_gen_3.out
+
+python /home/cliu3/PROTAI/baseline/data_gen.py data.partition=3
