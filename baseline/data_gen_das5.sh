e6ce2c6ed7b702daae59268836591fa1049ec054
congliuUvA <c.liu4@uva.nl>
2023-02-25 13:04:10 +0100

    generate 2/6

--- baseline/data_gen_das5.sh
+++ baseline/data_gen_das5.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=data_generation_1
+#SBATCH --job-name=data_generation_2
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=64G
 #SBATCH --time=7-0
-#SBATCH -o data_gen_1.out
+#SBATCH -o data_gen_2.out
 
 ray start --head
-python /var/scratch/cliu4/PROTAI/baseline/data_gen.py data.num_partition=6 data.partition_idx=1
+python /var/scratch/cliu4/PROTAI/baseline/data_gen.py data.num_partition=6 data.partition_idx=2
 
