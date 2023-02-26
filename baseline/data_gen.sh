6fe7d057010030c5985e2a909f4f10cd8522277b
congliuUvA <c.liu4@uva.nl>
2023-02-26 21:42:38 +0100

    test

--- baseline/data_gen.sh
+++ baseline/data_gen.sh
@@ -5,7 +5,6 @@
 #SBATCH --cpus-per-task=46
 #SBATCH --mem=64G
 #SBATCH --time=7-0
-#SBATCH -o data_gen_3.out
 
 ray start --head
 python /home/cliu3/PROTAI/baseline/data_gen.py data.num_partition=4 data.partition_idx=3
