6031efba6b13a11edc3d918165bd0228243a358f
congliuUvA <c.liu4@uva.nl>
2023-03-01 13:14:43 +0100

    Adam

--- baseline/train.py
+++ baseline/train.py
@@ -162,6 +162,8 @@ def get_optimizer(args_model, model):
 
     if args_model.optimizer == "SGD":
         optimizer = torch.optim.SGD(model.parameters(), lr=args_model.lr, momentum=0.75)
+        optimizer = torch.optim.Adam(model.parameters(), lr=args_model.lr)
+
         lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=7000, gamma=0.1)
 
     return optimizer, lr_scheduler
