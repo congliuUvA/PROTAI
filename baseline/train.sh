565391d9927c0d166f2396dc0b0a725e8039db62
congliuUvA <c.liu4@uva.nl>
2023-03-02 17:39:56 +0100

    add script

---
+++ baseline/train.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --job-name=train
+#SBATCH --tasks=1
+#SBATCH --cpus-per-task=46
+#SBATCH --gres=gpu:8
+#SBATCH --mem=120G
+#SBATCH --time=7-0
+#SBATCH --nodelist=ivi-cn001
+
+python /home/cliu3/PROTAI/baseline/train.py >train.out
+
