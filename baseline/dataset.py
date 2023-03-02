5fb55880dfe60f1e44b54f0208c5ec09651cb681
congliuUvA <c.liu4@uva.nl>
2023-03-02 09:57:27 +0100

    set sampler to True

--- baseline/dataset.py
+++ baseline/dataset.py
@@ -94,6 +94,7 @@ class VoxelsDataset(Dataset):
         Returns:
             Loaded data and its label
         """
+        print(idx)
         # given one specific data instance idx, find the corresponding position in
         # accumulated length list, the idx + 1 represents the index of the row in the csv file
         info = self.look_up_table[idx]
