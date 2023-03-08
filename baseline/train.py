8ae5496f2965cc5c9972ff5719407702b92a828a
congliuUvA <c.liu4@uva.nl>
2023-03-08 14:04:51 +0100

    debug:

--- baseline/train.py
+++ baseline/train.py
@@ -198,6 +198,7 @@ def main(args: DictConfig):
         num_workers=args_model.num_workers,
         pin_memory=True,
     )
+    print(val_set.freq)
 
     loss_func = nn.CrossEntropyLoss()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -231,6 +232,7 @@ def main(args: DictConfig):
             transform=transformation,
             use_sampler=args_model.use_sampler
         )
+        print(train_set.freq)
         if args_model.use_sampler:
             weighted_sampler = WeightedRandomSampler(
                 weights=train_set.proportion_list,
